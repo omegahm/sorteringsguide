5ecbb54f58d2f0cf38e4797fc4c123fa92748cc3
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-10-14 23:26:20 +0200

    Removed test/; Added home_controller specs

---
+++ spec/controllers/home_controller.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe HomeController do
+  describe '#index' do
+    before do
+      @rc = FactoryGirl.create(:recycling_center)
+    end
+
+    it 'renders' do
+      get :index
+      response.should be_success
+    end
+
+    it 'assigns' do
+      get :index
+      assigns(:last_recycling_center_updated).should == @rc.updated_at.iso8601
+      assigns(:recycling_centers).should == [@rc]
+    end
+  end
+
+  describe '#send_suggestion_mail' do
+    it 'sends to the correct mails' do
+      FactoryGirl.create(:admin)
+      FactoryGirl.create(:admin, mails: false)
+
+      put :send_suggestion_mail, { name: 'some', email: 'that', suggestion: 'yup' }
+      response.should be_success
+      ActionMailer::Base.deliveries.should_not be_empty
+      ActionMailer::Base.deliveries.count.should == 1
+    end
+  end
+end
