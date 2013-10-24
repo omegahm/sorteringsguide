404f092a47c79ad4e11875418856d0513bfe4175
Mads Ohm Larsen <mads.ohm@gmail.com>
2013-10-24 20:58:34 +0200

    Updated wrt. Henriks comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,12 @@
 //= require knockout
 //= require bootstrap
 //= require_tree .
+
+var collapsed = true;
+$("#missing").click(function() {
+  $("#send-suggestion-mail-collapse").collapse("toggle");
+});
+
+$('#send-suggestion-mail-collapse').on("shown.bs.collapse", function() {
+  $(window).scrollTop($('#send-suggestion-mail-collapse').offset().top);
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     @last_sign_updated ||= Sign.order(updated_at: :desc).select(:updated_at).first.updated_at.to_i
 
     @signs ||= Rails.cache.fetch("c_signs_#{@last_sign_updated}_#{@recycling_center.id}") do
-      Sign.for_factions(@factions, @recycling_center.category).select("*, (search_terms || ', ' || name) AS search_terms")
+      Sign.for_factions(@factions, @recycling_center.category).select("*, (search_terms || ', ' || name) AS search_terms").order(:name)
     end
   end
 end
